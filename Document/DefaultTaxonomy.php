8d8ea34448e14d3b6e58e0c02da087e884992070
Ivan Suárez <ivan@ulabox.com>
2013-02-18 13:35:24 +0100

    added mongodb DefaultTaxon and DefaultTaxonomy documents

---
+++ Document/DefaultTaxonomy.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\TaxonomiesBundle\Document;
+
+/**
+ * Default document for taxonomies.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ * @author Ivannis Suárez Jérez <ivannis.suarez@gmail.com>
+ */
+class DefaultTaxonomy extends Taxonomy
+{
+}
